d6c9baeb50ee7b191391bdc236119dff146d5eb0
Hector Rodrigues Da Silva <hectddos103@gmail.com>
2020-05-06 09:17:07 -0300

    adicionando animacao no cursor

---
+++ js/animationName.js
@@ -0,0 +1,28 @@
+(function () {
+    let message = "Hector";
+    let concludeMessage = "Silva =)";
+    new Promise((resolve, reject) => {
+        const typeWritter = (texto, idElemento, tempo) => {
+            let char = texto.split('').reverse();
+            let typer = setInterval(function () {
+                if (!char.length) return clearInterval(typer);
+                let next = char.pop();
+                document.getElementById(idElemento).innerHTML += next;
+            }, tempo);
+            setTimeout(()=>{
+                resolve();
+            }, 2000);
+        }
+        typeWritter(message, 'name', 200);
+    }).then(() => {
+        const typeWritter = (texto, idElemento, tempo) => {
+            let char = texto.split('').reverse();
+            let typer = setInterval(function () {
+                if (!char.length) return clearInterval(typer);
+                let next = char.pop();
+                document.getElementById(idElemento).innerHTML += next;
+            }, tempo);
+        }
+        typeWritter(concludeMessage, 'conclude__name', 100);
+    })
+}());
