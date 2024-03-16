6b197d14a31f0f7506f5b1db5e98300dc7d3aa14
Hector Rodrigues da Silva <hectddos103@gmail.com>
2024-03-16 18:50:02 -0300

    bugfix: version actions/setup-node and actions/checkout to @v4

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
